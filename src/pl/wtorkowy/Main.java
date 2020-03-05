f42b3d79cffd4ce0ae69966214bbf340c22f9756
kacper1410 <kacper.swiercz00@gmail.com>
2020-03-05 11:56:06 +0100

    Add multiply method

--- src/pl/wtorkowy/Main.java
+++ src/pl/wtorkowy/Main.java
@@ -1,8 +1,42 @@
 package pl.wtorkowy;
 
+import java.util.Arrays;
+
 public class Main {
 
+    public static byte[][] multiply(byte[][] first, byte[][] second) {
+        byte[][] result = new byte[first.length][second[0].length];
+
+        if(first[0].length != second.length)
+            return null;
+
+        for (int i = 0; i < first.length; i++) {
+            for (int j  = 0; j < second[0].length; j++) {
+                for (int k = 0; k < second.length; k++) {
+                    result[i][j] = (byte) (result[i][j] ^ first[i][k] * second[k][j]);
+                }
+            }
+        }
+
+
+        return result;
+    }
+
     public static void main(String[] args) {
+        byte[][] first = {
+                {1, 0, 0},
+                {1, 0, 1}
+        };
+        byte[][] second = {
+                {1, 0},
+                {1, 0},
+                {0, 1}
+        };
+        byte [][] result = multiply(first, second);
+        for (byte[] b :
+                result) {
+            System.out.println(Arrays.toString(b));
+        }
 	// write your code here
     }
 }
